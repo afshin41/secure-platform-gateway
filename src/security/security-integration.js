3e76d2fbc5eb446067b559e2e3607cdcad98bf3f
Afshin Saleh <afshin41@yahoo.com>
2026-08-19 15:18:38 +0330

    Expose audit persistence integration

--- src/security/security-integration.js
+++ src/security/security-integration.js
@@ -152,6 +152,7 @@ export function createSecurityIntegration(
         sessionGuard,
         signalingGuard,
         runtimePersistence,
+        auditPersistence,
 
         async initializePersistence(
             persistenceManager
